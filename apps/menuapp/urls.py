7b5e01b491a43bfbfbf2e55929111fee0024d1b9
Texia González <texia25.g@gmail.com>
2021-07-17 04:26:20 -0400

    added: urls to use login and logout

--- apps/menuapp/urls.py
+++ apps/menuapp/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from django.urls.resolvers import URLPattern
-from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('menu', views.home, name="home"),
+    path('', views.home, name="home"),
     path('menu/create', views.menu_create, name="menu_create"),
     path('menu/list', views.menu_list, name="menu_list"),
     path('menu/<pk>', views.menu_detail, name="menu_detail"),
@@ -14,4 +14,5 @@ urlpatterns = [
     path('menu/<menu_pk>/option/delete/<pk>', views.option_delete, name="delete_option"),
     path('order/list', views.order_list, name="order_list"),
     path('order/create/<menu_pk>/order/', views.order_create, name="order_create"),
+    path('accounts/login/', auth_views.LoginView.as_view(template_name='menuapp/login.html')),
 ]
