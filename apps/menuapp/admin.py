3751d2efe5ca807f731c22f15a66ac6cbdfc5a79
Texia González <texia25.g@gmail.com>
2021-07-17 04:30:18 -0400

    auth admin to order, option, menu

--- apps/menuapp/admin.py
+++ apps/menuapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Order, Option, Menu
 
 # Register your models here.
+admin.site.register(Order)
+admin.site.register(Option)
+admin.site.register(Menu)
